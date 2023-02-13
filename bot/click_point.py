eb006333302dd770ea81e93e5db33587a7546644
Marius R <marius.rekus@gmail.com>
2023-02-13 19:08:24 +0200

    Pataisytas delay, coords

--- bot/click_point.py
+++ bot/click_point.py
@@ -5,6 +5,6 @@ from random import uniform
 
 
 def click(x, y):
-    win32api.SetCursorPos((x+70, y+70))
+    win32api.SetCursorPos((x+20, y+70))
     time.sleep(round(uniform(0.1, 0.13), 2))
     pyautogui.click(clicks=1, interval=(round(uniform(0.1, 0.2), 2)))
--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from bot.click_point import click
 
 def main():
     while True:
-        time.sleep(round(uniform(0.08, 0.12), 2))
+        time.sleep(round(uniform(0.5, 1), 2))
         try:
             y, x = get_coordinates()
+            click(x, y)
         except:
             continue
-        click(x, y)
 
 
 if __name__ == "__main__":
