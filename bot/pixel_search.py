3d50f1ecd1db1a8f149172e736d3e8135d7fe003
Marius R <marius.rekus@gmail.com>
2023-02-13 18:02:44 +0200

    Sudėta koordinačių f-ja su apibrėžtu bbox ekrane

--- bot/pixel_search.py
+++ bot/pixel_search.py
@@ -2,17 +2,18 @@ from PIL import ImageGrab
 import cv2
 import numpy as np
 
-im = ImageGrab.grab(bbox=(565, 27, 1073, 357))
-opencvImage = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
-# im = cv2.imread('test.png')
-# im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
-coord = list()
-for j in range(0, opencvImage.shape[0]):
-    for i in range(0, opencvImage.shape[1]):
-        if opencvImage[j][i].tolist() == [0, 255, 0]:
-            coord.append([j, i])
-            print(coord[0])
-            break
-    else:
-        continue
-    break
+
+def get_coordinates(bbox=(565, 27, 1073, 357)):
+    im = ImageGrab.grab(bbox)
+    opencvImage = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+    coord = list()
+    for j in range(0, opencvImage.shape[0]):
+        for i in range(0, opencvImage.shape[1]):
+            if opencvImage[j][i].tolist() == [0, 255, 0]:
+                coord.append([j, i])
+                print(coord[0])
+                break
+        else:
+            continue
+        break
+    return coord
