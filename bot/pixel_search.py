c660c2d75552df1197429dc960cffadf51b12ba3
Marius R <marius.rekus@gmail.com>
2023-02-13 17:57:37 +0200

    Rastos pirmos [0, 255, 0] koordinatės ekrane

--- bot/pixel_search.py
+++ bot/pixel_search.py
@@ -4,12 +4,15 @@ import numpy as np
 
 im = ImageGrab.grab(bbox=(565, 27, 1073, 357))
 opencvImage = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+# im = cv2.imread('test.png')
+# im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
 coord = list()
-# print(opencvImage[1, 1])
-# print(type(opencvImage[1, 1]))
-# print(type(np.asarray([0, 255, 0])))
 for j in range(0, opencvImage.shape[0]):
     for i in range(0, opencvImage.shape[1]):
         if opencvImage[j][i].tolist() == [0, 255, 0]:
             coord.append([j, i])
-print(coord)
+            print(coord[0])
+            break
+    else:
+        continue
+    break
