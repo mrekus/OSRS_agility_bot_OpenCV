3aae61a3c8ae2273a56af05e54abf7484835473b
Marius R <marius.rekus@gmail.com>
2023-02-13 18:12:50 +0200

    Sudėta test main f-jos ciklas

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from bot.pixel_search import get_coordinates
+from bot.click_point import click
+
+
 def main():
-    pass
+    while True:
+        coords = get_coordinates()
+        click(coords)
 
 
 if __name__ == "__main__":
